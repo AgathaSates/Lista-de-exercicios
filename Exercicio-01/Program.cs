f81715d0fbeda60bd378e19ba62caf4a1bf1fbeb
Agatha Sates <satesagatha@gmail.com>
2025-03-20 18:07:18 -0300

    feat: adicionado Exercicio 01

---
+++ Exercicio-01/Program.cs
@@ -0,0 +1,34 @@
+﻿using Biblioteca;
+namespace Exercicio_01;
+
+internal class Program
+{ // 1. Crie um programa para calcular o volume de uma caixa retangular.
+    static void Main(string[] args)
+    {
+        Console.Title = "Volume de uma caixa retangular";
+        do {
+            Metodos.Titulo("Calcule o volume de uma caixa retangular");
+            Operacao();
+        } while (Metodos.Continuar());
+
+    }
+    
+    ////////////////////////////////////////////////////////////////////
+   
+    static void Operacao() 
+    {
+        Console.Write("-> Digite o comprimento da caixa: ");
+        double comprimento = Metodos.ValorValidoDouble();
+
+        Console.Write("-> Digite a largura da caixa: ");
+        double largura = Metodos.ValorValidoDouble();
+
+        Console.Write("-> Digite a altura da caixa: ");
+        double altura = Metodos.ValorValidoDouble();
+
+        double resultado = comprimento * largura * altura;
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine($" -> O volume da caixa é de {resultado} cm³.");
+        Console.WriteLine("----------------------------------------------");
+    }       
+}
