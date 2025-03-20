1949f402365a1e77826e391218f76dba68885c4c
Agatha Sates <satesagatha@gmail.com>
2025-03-20 20:37:56 -0300

    feat: adicionado Exercicio 02

---
+++ Exercicio-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using Biblioteca;
+
+namespace Exercicio_02;
+
+internal class Program
+{
+    //2. Crie um programa para calcular o volume de um Cilindro.
+    static void Main(string[] args)
+    {
+        Console.Title = "Volume de um Cilindro";
+        do { 
+            Metodos.Titulo("Calcule o volume de um cilindro");
+            Operacao();
+        } while (Metodos.Continuar());
+    }
+    static void Operacao()
+    {
+        Console.Write("-> Digite o raio do cilindro: ");
+        double raio = Metodos.ValorValidoDouble();
+        Console.Write("-> Digite a altura do cilindro: ");
+        double altura = Metodos.ValorValidoDouble();
+        double resultado = Math.PI * Math.Pow(raio, 2) * altura;
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine($" -> O volume do cilindro é de {resultado:F2} cm³.");
+        Console.WriteLine("----------------------------------------------");
+    }
+}
