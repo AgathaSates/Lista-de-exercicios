44a36d582fe320fe3c67f3b6d0b7b8a234de4cd6
Agatha Sates <satesagatha@gmail.com>
2025-03-21 04:54:06 -0300

    feat: Adicionado Exercicio-08

---
+++ Exercicio-08/Program.cs
@@ -0,0 +1,55 @@
+﻿using Biblioteca;
+
+namespace Exercicio_08;
+
+internal class Program
+{
+    //8. Crie um programa para verificar se um número é primo.
+    static void Main(string[] args)
+    {
+        Console.Title = "Verificador de número primo";
+        do
+        {
+            Metodos.Titulo("    Verificador de número primo");
+            Operacao();
+        } while (Metodos.Continuar());
+
+        static void Operacao()
+        {
+
+            Console.Write("-> Digite um número: ");
+            int numero = Metodos.ValorValidoInt();
+
+            while (numero <= 1)
+            {
+                Console.WriteLine(" -> (X) O número deve ser maior que 1.");
+                Console.Write("-> Digite um número novamente: ");
+                numero = Metodos.ValorValidoInt();
+            }
+
+            bool primo = true;
+                                              // confere entre 2 e -1 do {numero} para saber se ele tem mais que 2 divisores.
+            for (int i = 2; i < numero; i++) // i = 2 pq 1 divide qualquer número./ testa se o {numero} é divisível por i.
+            {      // se for <= {numero}, ele entraria no if, e resultaria em false pq todos os numeros são divisíveis por ele mesmo. 
+                if (numero % i == 0) // Se o resto da divisão for 0, o {numero} foi dividido por outro q n seja 1 ou ele.
+                {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (!primo)
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine($"-> O número {numero} > NÃO < é primo.");
+                Console.WriteLine("-------------------------------------------------------");
+            }
+            else 
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine($"-> O número {numero} é primo.");
+                Console.WriteLine("-------------------------------------------------------");
+            }
+        }
+    }
+}
