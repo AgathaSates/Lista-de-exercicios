2ee4072fa3f3b1d89bef15ed5dc9bb067afb80c6
Agatha Sates <satesagatha@gmail.com>
2025-03-22 17:20:08 -0300

    feat: adicionado Exercicio 18

---
+++ Exercicio-18/Program.cs
@@ -0,0 +1,29 @@
+﻿using Biblioteca;
+
+namespace Exercicio_18;
+
+internal class Program
+{
+    //18. Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+    static void Main(string[] args)
+    {
+        Console.Title = "Números ímpares";
+
+        Metodos.Titulo("   Números ímpares entre 100 e 200 ");
+        Operacao();
+
+        static void Operacao()
+        {
+            Console.WriteLine("Lista dos números");
+            Console.WriteLine("------------------");
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine($"- {i}");
+
+            }
+            Console.WriteLine("------------------");
+
+        }
+    }
+}
