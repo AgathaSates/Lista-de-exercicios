0ec19df40bb17336ea69534ce830a27b29632432
Agatha Sates <satesagatha@gmail.com>
2025-03-22 15:52:00 -0300

    feat: adicionado Exercicio 13

---
+++ Exercicio-13/Program.cs
@@ -0,0 +1,54 @@
+﻿using Biblioteca;
+
+namespace Exercicio_13;
+
+internal class Program
+{
+    //13. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+    static void Main(string[] args)
+    {
+        Console.Title = "Soma de A + B é menor que C?";
+        do
+        {
+            Metodos.Titulo("          Soma de A + B é menor que C?");
+            Operacao();
+        } while (Metodos.Continuar());
+
+        static void Operacao() 
+        {
+            Console.Write("-> Digite o valor de A: ");
+            double a = Metodos.ValorValidoDouble();
+
+            Console.Write("-> Digite o valor de B: ");
+            double b = Metodos.ValorValidoDouble();
+
+            Console.Write("-> Digite o valor de C: ");
+            double c = Metodos.ValorValidoDouble();
+
+            double soma = a + b;
+            
+            if (soma < c)
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($" -> A soma de A + B é: {soma}.");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($" -> O valor de C é: {c}.");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("-> A soma de A + B é menor que C!");
+                Console.WriteLine("-------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($" -> A soma de A + B é: {soma}");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($" -> O valor de C é: {c}");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine($"-> A soma de A + B >NÂO< é menor que C!");
+                Console.WriteLine("-------------------------------------");
+
+            }
+
+        }
+    }
+}
